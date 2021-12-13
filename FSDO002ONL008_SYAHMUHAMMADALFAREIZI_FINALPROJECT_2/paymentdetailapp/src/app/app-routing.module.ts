e46b8169d9bef3039254996b9bc404d3d1862518
Syah Muhammad Alfareizi <contact.syahfareizi@gmail.com>
2021-12-14 00:45:26 +0700

    Belajar Angular

---
+++ FSDO002ONL008_SYAHMUHAMMADALFAREIZI_FINALPROJECT_2/paymentdetailapp/src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { LoginpageComponent } from './components/loginpage/loginpage.component';
+import { RegisterpageComponent } from './components/registerpage/registerpage.component';
+import { AuthguardGuard } from './services/authguard.guard';
+
+const routes: Routes = [
+  {path:'',redirectTo:'/homepage', pathMatch:'full'},
+  {path:'register', component:RegisterpageComponent},
+  {path:'login', component:LoginpageComponent},
+  {path:'homepage', component:HomepageComponent , canActivate:[AuthguardGuard]}
+
+  // {path:'homepage',component:GetemployeeComponent},
+  // {path:'updateemployee/:id',component:UpdateemployeeComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ FSDO002ONL008_SYAHMUHAMMADALFAREIZI_FINALPROJECT_2/paymentdetailapp/src/app/components/updatepage/updatepage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-updatepage',
+  templateUrl: './updatepage.component.html',
+  styleUrls: ['./updatepage.component.css']
+})
+export class UpdatepageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
