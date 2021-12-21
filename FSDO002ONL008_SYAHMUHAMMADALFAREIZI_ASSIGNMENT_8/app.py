55835d24a439fbcdc6d3fa53d2e5934c6d079fc7
Syah Muhammad Alfareizi <contact.syahfareizi@gmail.com>
2021-12-21 09:30:38 +0700

    Belajar Python

---
+++ FSDO002ONL008_SYAHMUHAMMADALFAREIZI_ASSIGNMENT_8/app.py
@@ -0,0 +1,43 @@
+# DAFTAR FUNGSI
+def celciustokelvin(suhuawal):
+    '''Fungsi untuk mengkonversi suhu dari Celcius ke Kelvin'''
+    suhukelvin = suhuawal + 273.15
+    return {"celcius": suhuawal, "kelvin": suhukelvin}
+
+
+def kelvintocelcius(suhuawal):
+    '''Fungsi untuk mengkonversi suhu dari Kelvin ke Celcius'''
+    suhucelcius = suhuawal - 273.15
+    return {"kelvin": suhuawal, "celcius": suhucelcius}
+
+
+def tofahrenheit(suhuawalcelcius, suhuawalkelvin):
+    '''Fungsi untuk menkonversi suhu menuju Fahrenheit'''
+    suhufahrenheit1 = ((9/5)*suhuawalcelcius)+32
+    suhufahrenheit2 = (suhuawalkelvin - 273.15)*(9/5)+32
+    return {"celcius": suhuawalcelcius, "kelvin": suhuawalkelvin, "celcius_to_fahrenheit": suhufahrenheit1, "kelvin_to_fahrenheit": suhufahrenheit2}
+
+
+# APP HEADER
+print("<---------------------------------------->")
+print("SELAMAT DATANG PADA APLIKASI KONVERSI SUHU")
+print("<---------------------------------------->")
+
+# INPUT USER
+suhuawal = input("Masukkan Suhu : ")
+suhuawal_int = float(suhuawal)
+hasilsuhu_kelvin = celciustokelvin(suhuawal_int)
+hasilsuhu_celcius = kelvintocelcius(suhuawal_int)
+hasilsuhu_fahrenheit = tofahrenheit(
+    hasilsuhu_kelvin["celcius"], hasilsuhu_celcius["kelvin"])
+
+# TAMPILAN
+print("<---------------------------------------->")
+print(celciustokelvin.__doc__)
+print(hasilsuhu_kelvin)
+print("<---------------------------------------->")
+print(kelvintocelcius.__doc__)
+print(hasilsuhu_celcius)
+print("<---------------------------------------->")
+print(tofahrenheit.__doc__)
+print(hasilsuhu_fahrenheit)
---
+++ python/sesi3/app.py
@@ -0,0 +1,6 @@
+def my_fun(p,l):
+    '''This prints a passed string into this function'''
+    print(p*l)
+
+my_fun(3,5)
+print(my_fun.__doc__)
