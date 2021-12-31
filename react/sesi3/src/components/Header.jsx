bccd69be30ed365436844ae824ebdf5a758ecc6c
Syah Muhammad Alfareizi <contact.syahfareizi@gmail.com>
2021-12-31 10:06:14 +0700

    Belajar React

---
+++ react/sesi3/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Component } from "react";
+
+class Header extends Component{
+    render(){
+        return( 
+        <h3>
+            FSD OCBC NISP BATCH II
+        </h3>
+        )
+    }
+}
+
+export default Header
