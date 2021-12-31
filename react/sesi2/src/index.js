fc9a03a12c171a01b88296530c5e35e5e48242ac
Syah Muhammad Alfareizi <contact.syahfareizi@gmail.com>
2021-12-31 09:14:38 +0700

    Belajar React

---
+++ react/sesi2/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const HellowWorld = () => {
+    return (
+        <h1> Hello World</h1>
+    );
+}
+
+ReactDOM.render(<HellowWorld/>,
+    document.getElementById("root"));
