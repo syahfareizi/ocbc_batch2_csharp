3beecd5bc9aaed90a57926ff7c6c4fed445f5ce4
Syah Muhammad Alfareizi <contact.syahfareizi@gmail.com>
2022-01-03 11:25:13 +0700

    Belajar React

--- react/sesi5/src/App.js
+++ react/sesi5/src/App.js
@@ -1,43 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
-function Header () {
-  return (
-    <header classname="header">
-      <h1 >My First Header </h1>
-    </header>
-  )
-}
-function Content () {
-  return (
-    <div classname="content">
-      <p >
-      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-      </p>
-    </div>
-  )
-}
-function Footer () {
-  return (
-    <div classname="footer">
-      <p>
-        &copy; My Self -2201
-      </p>
-    </div>
-  )
-}
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Content from "./components/Content";
 
 function App() {
   return (
-    <div classname="App">
-      <Header></Header>
+    <div className="App">
+      <Header />
       <hr></hr>
-      <Content></Content>
+      <Content/>
       <hr></hr>
-      <Footer></Footer>
+      <Footer/>
       <hr></hr>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
