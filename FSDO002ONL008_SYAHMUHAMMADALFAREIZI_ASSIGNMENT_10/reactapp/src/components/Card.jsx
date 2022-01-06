1a78a031fb1fe9886f22717c7d61dd251cea0e56
Syah Muhammad Alfareizi <contact.syahfareizi@gmail.com>
2022-01-06 09:44:48 +0700

    Belajar React

--- FSDO002ONL008_SYAHMUHAMMADALFAREIZI_ASSIGNMENT_10/reactapp/src/components/Card.jsx
+++ FSDO002ONL008_SYAHMUHAMMADALFAREIZI_ASSIGNMENT_10/reactapp/src/components/Card.jsx
@@ -4,15 +4,18 @@ function Card(props) {
   const visitors = Array.from(props.visitors);
   return (
     <>
-      <div className="m-3 rounded bg-light p-2 text-center"> {"<"}== </div>
+      <div className=" m-3 rounded bg-light p-2 text-center"> {"<"}== </div>
       {props.visitors.length === 0 ? (
         <div className="container m-3 bg-warning rounded p-2 text-center">
           {" "}
           Tidak ada antrian !{" "}
         </div>
       ) : (
-        visitors.map((data) => (
-          <div className="container m-3 bg-warning rounded p-2 text-center">
+        visitors.map((data, index) => (
+          <div
+            key={index}
+            className="container m-3 bg-warning rounded p-2 text-center"
+          >
             {data}
           </div>
         ))
